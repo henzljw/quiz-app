0ef867bc1bef576b655ba11140e307b32acd3ae2
hezagon <hezagon99@gmail.com>
2021-08-06 18:34:48 +0800

    Added some basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,35 @@ void main() {
 }
 
 class MyQuiz extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'what\'s your favourite animal?',
+    ];
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
