40f09d534bb60af0724feb81f0823aa0c47d299b
hezagon <hezagon99@gmail.com>
2021-08-09 20:22:15 +0800

    Added stateful and stateless widget + styling + mapping

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.green,
+        ),
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,65 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
 void main() {
   runApp(MyQuiz());
 }
 
-class MyQuiz extends StatelessWidget {
+class MyQuiz extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyQuizState();
+  }
+}
+
+class _MyQuizState extends State<MyQuiz> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favourite color?',
-      'what\'s your favourite animal?',
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favourite instructor?',
+        'answers': ['Max', 'Max', 'Max', 'Max'],
+      },
+      {
+        'questionText': 'Where did you come from?',
+        'answers': ['Malaysia', 'Singapore', 'Indonesia', 'Vietnam'],
+      },
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('Quiz App'),
         ),
         body: Column(
           children: [
-            Text('The Question!'),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: null,
+            Question(
+              questions[_questionIndex]['questionText'] as String,
             ),
-            ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: null,
-            ),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: null,
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
           ],
         ),
       ),
